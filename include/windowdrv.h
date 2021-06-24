6ce72f71aba078ae1e8919db4761128ae4425cf0
Seeky <seeky.mkw@gmail.com>
2021-06-24 16:43:25 +0100

    Add partial windowdrv

---
+++ include/windowdrv.h
@@ -0,0 +1,47 @@
+#ifndef WINDOWDRV_H
+#define WINDOWDRV_H
+
+#include <common.h>
+
+struct _WindowEntry;
+
+typedef void (WindowFunc)(struct _WindowEntry * );
+
+typedef struct _WindowEntry {
+    u8 unknown_0x0[0x2 - 0x0];
+    u16 flags;
+    u16 pri; // uncertain
+    u8 unknown_0x6[0x8 - 0x6];
+    s32 unknown_0x8;
+    u8 unknown_0xc[0x28 - 0xc];
+    u32 unknown_0x28;
+    u8 unknown_0x2c[0x3c - 0x2c];
+    WindowFunc * mainFunc;
+    WindowFunc * deleteFunc;
+    u8 unknown_0x44[0x48 - 0x44];
+} WindowEntry;
+
+#define WINDOW_MAX 7
+
+void windowInit();
+void windowReInit();
+s32 windowEntry(u16 pri);
+bool windowDelete(WindowEntry * entry);
+bool windowDeleteID(s32 id);
+void windowMain();
+// unknown_80038b08
+// unknown_80038cc0
+// unknown_80038fb8
+// unknown_80039188
+// unknown_800393c8
+// unknown_800396e0
+// unknown_80039b80
+// unknown_80039d40
+// unknown_8003a440
+// unknown_8003a5e4
+// windowDispGX2_Waku_col
+s32 windowCheckID(s32 id);
+WindowEntry * windowGetPointer(s32 id);
+bool unknown_8003b180();
+
+#endif
---
+++ src/windowdrv.c
@@ -0,0 +1,89 @@
+#include <common.h>
+#include <memory.h>
+#include <string.h>
+#include <windowdrv.h>
+
+static WindowEntry * wp;
+
+void windowInit() {
+    WindowEntry * entries = __memAlloc(0, sizeof(WindowEntry[WINDOW_MAX]));
+    wp = entries;
+    for (s32 i = 0; i < WINDOW_MAX; i++) {
+        entries[i].flags = 0;
+        entries[i].unknown_0x28 = 0;
+    }
+}
+
+void windowReInit() {
+    WindowEntry * entries = wp;
+    for (s32 i = 0; i < WINDOW_MAX; i++) {
+        entries[i].flags = 0;
+        entries[i].unknown_0x28 = 0;
+    }
+}
+
+s32 windowEntry(u16 pri) {
+    WindowEntry * entry = wp;
+    for (s32 i = 0; i < WINDOW_MAX; i++, entry++) {
+        if ((entry->flags & 1) == 0) {
+            memset(entry, 0, sizeof(*entry));
+            entry->flags = 1;
+            entry->pri = pri;
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool windowDelete(WindowEntry * entry) {
+    if (entry->deleteFunc != NULL)
+        entry->deleteFunc(entry);
+    entry->flags = 0;
+    entry->unknown_0x28 = 0;
+    return true;
+}
+
+bool windowDeleteID(s32 id) {
+    WindowEntry * entry = &wp[id];
+    if ((entry->flags & 1) == 0)
+        return false;
+    else
+        return windowDelete(entry);
+}
+
+void windowMain() {
+    WindowEntry * entry = wp;
+    for (s32 i = 0; i < WINDOW_MAX; i++, entry++) {
+        if ((entry->flags & 1) && (entry->mainFunc != NULL))
+            entry->mainFunc(entry);
+    }
+}
+
+// unknown_80038b08
+// unknown_80038cc0
+// unknown_80038fb8
+// unknown_80039188
+// unknown_800393c8
+// unknown_800396e0
+// unknown_80039b80
+// unknown_80039d40
+// unknown_8003a440
+// unknown_8003a5e4
+// windowDispGX2_Waku_col
+
+s32 windowCheckID(s32 id) {
+    return (int) wp[id].flags & 2;
+}
+
+WindowEntry * windowGetPointer(s32 id) {
+    return &wp[id];
+}
+
+bool unknown_8003b180() {
+    WindowEntry * entry = wp;
+    for (s32 i = 0; i < WINDOW_MAX; i++, entry++) {
+        if ((entry->flags & 1) && (entry->unknown_0x8 != 1))
+            return true;
+    }
+    return false;
+}
