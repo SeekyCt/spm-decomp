244819e3bb13ca327c46a25ca7f52f51b6b6b096
Seeky <58006653+SeekyCt@users.noreply.github.com>
2023-08-21 17:52:41 +0100

    Remove outdated union

--- spm-headers/include/spm/dvdmgr.h
+++ spm-headers/include/spm/dvdmgr.h
@@ -21,15 +21,10 @@ typedef struct
 {
 /* 0x00 */ char path[64];
 /* 0x40 */ DVDFileInfo fileInfo;
-union {
-    s32 temp[4];
-    struct {
-    /* 0x7C */ void * dest;
-    /* 0x80 */ s32 lengthRemaining;
-    /* 0x84 */ s32 offset;
-    /* 0x88 */ s32 lengthRead;
-    };
-};
+/* 0x7C */ void * dest;
+/* 0x80 */ s32 lengthRemaining;
+/* 0x84 */ s32 offset;
+/* 0x88 */ s32 lengthRead;
 /* 0x8C */ DVDMgrCallback * readCallback;
 /* 0x90 */ u16 flags;
 /* 0x92 */ u16 priority;
