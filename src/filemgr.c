7886bd69be88519154ed358ed44a6db4d99cfc5c
Seeky <58006653+SeekyCt@users.noreply.github.com>
2025-01-11 22:00:12 +0000

    Cleanup unpack/packtexpalette

--- src/filemgr.c
+++ src/filemgr.c
@@ -56,38 +56,38 @@ void UnpackTexPalette(TPLHeader * palette)
 
     VALIDATE_TPL_VERSION(0x35, palette);
 
-    if (IS_TPL_PACKED(palette))
+    if (!IS_TPL_PACKED(palette))
+        return;
+
+    // Convert image table offset to pointer
+    palette->imageTable = (ImageTableEntry *) (palette->imageTableOffset + (u32) palette);
+
+    // Unpack all images in table
+    for (i = 0; i < palette->imageCount; i++)
     {
-        // Convert image table offset to pointer
-        palette->imageTable = (ImageTableEntry *) (palette->imageTableOffset + (u32) palette);
-
-        // Unpack all images in table
-        for (i = 0; i < palette->imageCount; i++)
+        if (palette->imageTable[i].imageOffset != 0)
         {
-            if (palette->imageTable[i].imageOffset != 0)
-            {
-                // Convert image header offset to pointer
-                palette->imageTable[i].image = (ImageHeader *) ((u32) palette + palette->imageTable[i].imageOffset);
+            // Convert image header offset to pointer
+            palette->imageTable[i].image = (ImageHeader *) ((u32) palette + palette->imageTable[i].imageOffset);
 
-                if (!palette->imageTable[i].image->unpacked)
-                {
-                    // Convert image data offset to pointer
-                    palette->imageTable[i].image->data = (void *) ((u32) palette + palette->imageTable[i].image->dataOffset);
-                    palette->imageTable[i].image->unpacked = true;
-                }
+            if (!palette->imageTable[i].image->unpacked)
+            {
+                // Convert image data offset to pointer
+                palette->imageTable[i].image->data = (void *) ((u32) palette + palette->imageTable[i].image->dataOffset);
+                palette->imageTable[i].image->unpacked = true;
             }
+        }
 
-            if (palette->imageTable[i].paletteOffset != 0)
+        if (palette->imageTable[i].paletteOffset != 0)
+        {
+            // Convert palette header offset to pointer
+            palette->imageTable[i].palette = (PaletteHeader *) ((u32) palette + palette->imageTable[i].paletteOffset);
+
+            if (!palette->imageTable[i].palette->unpacked)
             {
-                // Convert palette header offset to pointer
-                palette->imageTable[i].palette = (PaletteHeader *) ((u32) palette + palette->imageTable[i].paletteOffset);
-
-                if (!palette->imageTable[i].palette->unpacked)
-                {
-                    // Convert palette data offset to pointer
-                    palette->imageTable[i].palette->data = (void *) ((u32) palette + palette->imageTable[i].palette->dataOffset);
-                    palette->imageTable[i].palette->unpacked = true;
-                }
+                // Convert palette data offset to pointer
+                palette->imageTable[i].palette->data = (void *) ((u32) palette + palette->imageTable[i].palette->dataOffset);
+                palette->imageTable[i].palette->unpacked = true;
             }
         }
     }
@@ -99,42 +99,42 @@ void PackTexPalette(TPLHeader * palette)
 
     VALIDATE_TPL_VERSION(0x5e, palette);
 
-    if(!IS_TPL_PACKED(palette))
+    if(IS_TPL_PACKED(palette))
+        return;
+
+    // Pack all images in table
+    for (i = 0; i < palette->imageCount; i++)
     {
-        // Pack all images in table
-        for (i = 0; i < palette->imageCount; i++)
+        if (palette->imageTable[i].image != NULL)
         {
-            if (palette->imageTable[i].image != NULL)
+            if (palette->imageTable[i].image->unpacked)
             {
-                if (palette->imageTable[i].image->unpacked)
-                {
-                    palette->imageTable[i].image->unpacked = false;
+                palette->imageTable[i].image->unpacked = false;
 
-                    // Convert image data pointer to offset
-                    palette->imageTable[i].image->dataOffset = (u32) palette->imageTable[i].image->data - (u32) palette;
-                }
-
-                // Convert image header pointer to offset
-                palette->imageTable[i].imageOffset = (u32) palette->imageTable[i].image - (u32) palette;
+                // Convert image data pointer to offset
+                palette->imageTable[i].image->dataOffset = (u32) palette->imageTable[i].image->data - (u32) palette;
             }
 
-            if (palette->imageTable[i].palette != NULL)
-            {
-                if (palette->imageTable[i].palette->unpacked)
-                {
-                    palette->imageTable[i].palette->unpacked = false;
-
-                    // Convert palette data pointer to offset
-                    palette->imageTable[i].palette->dataOffset = (u32) palette->imageTable[i].palette->data - (u32) palette;
-                }
-
-                // Convert palette header pointer to offset
-                palette->imageTable[i].paletteOffset = (u32) palette->imageTable[i].palette - (u32) palette;
-            }
+            // Convert image header pointer to offset
+            palette->imageTable[i].imageOffset = (u32) palette->imageTable[i].image - (u32) palette;
         }
-        // Convert image table pointer to offset
-        palette->imageTableOffset = (u32) palette->imageTable - (u32) palette;
+
+        if (palette->imageTable[i].palette != NULL)
+        {
+            if (palette->imageTable[i].palette->unpacked)
+            {
+                palette->imageTable[i].palette->unpacked = false;
+
+                // Convert palette data pointer to offset
+                palette->imageTable[i].palette->dataOffset = (u32) palette->imageTable[i].palette->data - (u32) palette;
+            }
+
+            // Convert palette header pointer to offset
+            palette->imageTable[i].paletteOffset = (u32) palette->imageTable[i].palette - (u32) palette;
+        }
     }
+    // Convert image table pointer to offset
+    palette->imageTableOffset = (u32) palette->imageTable - (u32) palette;
 }
 
 // NOT_DECOMPILED fileGarbageDataAdrClear
